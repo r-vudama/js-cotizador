c31463f82bf7bd70996630d598bd0ffa6f9cc0b7
Ramon Urchipia <ramonurchipia@gmail.com>
2020-09-09 17:57:34 -0300

    arreglo funcion agregar articulo

--- main.js
+++ main.js
@@ -3,10 +3,63 @@ class Articulo {
         this.marca = marca;
         this.valor = valor;
         this.cantidad = cantidad;
+
+        this.agregarArticulo = function (){
+
+            const subtotal = Number(this.valor) * Number(this.cantidad);
+
+            const nuevoItem = document.createElement('div');
+            nuevoItem.setAttribute('id', 'nuevoItem');
+            const nuevoProducto = document.createElement('p');
+            const nuevoValor = document.createElement('p');
+            const nuevoCantidad = document.createElement('p');
+            const textoSubtotal = document.createElement('p');
+            const subtotalProducto = document.createElement('span');
+            const nuevoQuitar = document.createElement('p');
+
+            const elementoProducto = document.createTextNode(`Producto: ${this.marca} `);
+            const elementoValor = document.createTextNode(`Valor: $${this.valor}`);
+            const elementoCantidad = document.createTextNode(`Cantidad: ${this.cantidad} `);
+            const elementoTextoSubtotal = document.createTextNode('Subtotal: $');
+            const elementoSubtotal = document.createTextNode(subtotal);
+            const elementoQuitar = document.createTextNode('Quitar producto');
+
+                nuevoItem.appendChild(nuevoProducto);
+                nuevoProducto.setAttribute('id', 'nuevoProducto');
+                nuevoProducto.appendChild(elementoProducto);
+
+                nuevoItem.appendChild(nuevoValor);
+                nuevoValor.setAttribute('id', 'nuevoValor');
+                nuevoValor.appendChild(elementoValor);
+
+                nuevoItem.appendChild(nuevoCantidad);
+                nuevoCantidad.setAttribute('id', 'nuevoCantidad');
+                nuevoCantidad.appendChild(elementoCantidad);
+
+                nuevoItem.appendChild(textoSubtotal);
+                textoSubtotal.setAttribute('id', 'nuevoSubtotal');
+                textoSubtotal.appendChild(elementoTextoSubtotal);
+
+                textoSubtotal.appendChild(subtotalProducto);
+                subtotalProducto.setAttribute('name', 'subtotalProducto');
+                subtotalProducto.appendChild(elementoSubtotal);
+
+                nuevoItem.appendChild(nuevoQuitar);
+                nuevoQuitar.setAttribute('onclick', 'eliminarProducto(this)');
+                nuevoQuitar.setAttribute('class', 'quitarProducto');
+                nuevoQuitar.appendChild(elementoQuitar);
+
+            carrito.appendChild(nuevoItem)
+
+        calcularTotal();
+
+        }
     }
 }
-const epiphoneLesPaul = new Articulo('Epiphone Les Paul', 25000, 1);
-const epiphoneSG = new Articulo('Epiphone SG', 23500, 1);
+const epiphoneLesPaul = new Articulo('Epiphone Les Paul', 82134, 1);
+const epiphoneSG = new Articulo('Epiphone SG', 67137, 1);
+const squierJazz = new Articulo('Squier Jazz Bass', 69088, 1);
+const squierPrecision = new Articulo('Squier CV Precision', 82932, 1);
 
 
 
@@ -17,101 +70,13 @@ for (let i = 0 ; i < botones.length ; i++) {
     botones[i].addEventListener("click", function(){
         
         if(botones[i] == botones[0]){
-
-            const subtotal = Number(epiphoneLesPaul.valor) * Number(epiphoneLesPaul.cantidad);
-
-            const nuevoItem = document.createElement('div');
-            nuevoItem.setAttribute('id', 'nuevoItem');
-            const nuevoProducto = document.createElement('p');
-            const nuevoValor = document.createElement('p');
-            const nuevoCantidad = document.createElement('p');
-            const textoSubtotal = document.createElement('p');
-            const subtotalProducto = document.createElement('span');
-            const nuevoQuitar = document.createElement('p');
-
-            const elementoProducto = document.createTextNode(`Producto: ${epiphoneLesPaul.marca} `);
-            const elementoValor = document.createTextNode(`Valor: $${epiphoneLesPaul.valor}`);
-            const elementoCantidad = document.createTextNode(`Cantidad: ${epiphoneLesPaul.cantidad} `);
-            const elementoTextoSubtotal = document.createTextNode('Subtotal: $');
-            const elementoSubtotal = document.createTextNode(subtotal);
-            const elementoQuitar = document.createTextNode('Quitar producto');
-
-                nuevoItem.appendChild(nuevoProducto);
-                nuevoProducto.setAttribute('id', 'nuevoProducto');
-                nuevoProducto.appendChild(elementoProducto);
-
-                nuevoItem.appendChild(nuevoValor);
-                nuevoValor.setAttribute('id', 'nuevoValor');
-                nuevoValor.appendChild(elementoValor);
-
-                nuevoItem.appendChild(nuevoCantidad);
-                nuevoCantidad.setAttribute('id', 'nuevoCantidad');
-                nuevoCantidad.appendChild(elementoCantidad);
-
-                nuevoItem.appendChild(textoSubtotal);
-                textoSubtotal.setAttribute('id', 'nuevoSubtotal');
-                textoSubtotal.appendChild(elementoTextoSubtotal);
-
-                textoSubtotal.appendChild(subtotalProducto);
-                subtotalProducto.setAttribute('name', 'subtotalProducto');
-                subtotalProducto.appendChild(elementoSubtotal);
-
-                nuevoItem.appendChild(nuevoQuitar);
-                nuevoQuitar.setAttribute('onclick', 'eliminarProducto(this)');
-                nuevoQuitar.setAttribute('class', 'quitarProducto');
-                nuevoQuitar.appendChild(elementoQuitar);
-
-            carrito.appendChild(nuevoItem)
-
-        calcularTotal();
-
+            epiphoneLesPaul.agregarArticulo();
         } else if (botones[i] == botones[1]){
-            const subtotal = Number(epiphoneSG.valor) * Number(epiphoneSG.cantidad);
-
-            const nuevoItem = document.createElement('div');
-            nuevoItem.setAttribute('id', 'nuevoItem');
-            const nuevoProducto = document.createElement('p');
-            const nuevoValor = document.createElement('p');
-            const nuevoCantidad = document.createElement('p');
-            const textoSubtotal = document.createElement('p');
-            const subtotalProducto = document.createElement('span');
-            const nuevoQuitar = document.createElement('p');
-
-            const elementoProducto = document.createTextNode(`Producto: ${epiphoneSG.marca} `);
-            const elementoValor = document.createTextNode(`Valor: $${epiphoneSG.valor}`);
-            const elementoCantidad = document.createTextNode(`Cantidad: ${epiphoneSG.cantidad} `);
-            const elementoTextoSubtotal = document.createTextNode('Subtotal: $');
-            const elementoSubtotal = document.createTextNode(subtotal);
-            const elementoQuitar = document.createTextNode('Quitar producto');
-
-                nuevoItem.appendChild(nuevoProducto);
-                nuevoProducto.setAttribute('id', 'nuevoProducto');
-                nuevoProducto.appendChild(elementoProducto);
-
-                nuevoItem.appendChild(nuevoValor);
-                nuevoValor.setAttribute('id', 'nuevoValor');
-                nuevoValor.appendChild(elementoValor);
-
-                nuevoItem.appendChild(nuevoCantidad);
-                nuevoCantidad.setAttribute('id', 'nuevoCantidad');
-                nuevoCantidad.appendChild(elementoCantidad);
-
-                nuevoItem.appendChild(textoSubtotal);
-                textoSubtotal.setAttribute('id', 'nuevoSubtotal');
-                textoSubtotal.appendChild(elementoTextoSubtotal);
-
-                textoSubtotal.appendChild(subtotalProducto);
-                subtotalProducto.setAttribute('name', 'subtotalProducto');
-                subtotalProducto.appendChild(elementoSubtotal);
-
-                nuevoItem.appendChild(nuevoQuitar);
-                nuevoQuitar.setAttribute('onclick', 'eliminarProducto(this)');
-                nuevoQuitar.setAttribute('class', 'quitarProducto');
-                nuevoQuitar.appendChild(elementoQuitar);
-
-            carrito.appendChild(nuevoItem)
-
-        calcularTotal();
+            epiphoneSG.agregarArticulo();
+        } else if (botones[i] == botones[2]){
+            squierJazz.agregarArticulo();
+        } else if (botones[i] == botones[3]){
+            squierPrecision.agregarArticulo();
         }
     })
 
