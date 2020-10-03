ea3e2896dd3a6f2ae1d906beae17a7484a657d65
Ramon Urchipia <ramonurchipia@gmail.com>
2020-10-03 12:44:10 -0300

    total compra

--- js/compra.js
+++ js/compra.js
@@ -1,23 +1,25 @@
+const total = document.querySelector('#total');
 let productosLS = localStorage.getItem('carrito');
-let productosJson = JSON.parse(productosLS)
+let productosJson = JSON.parse(productosLS);
 
-const listaJson = document.getElementById("listado"); 
+const listaJson = document.querySelector('#listado'); 
 function agregarElementos(){ 
     
     productosJson.forEach (function (dataJson) {
     const subtotal = Number(dataJson.valor) * Number(dataJson.cantidad);
-    var item = document.createElement("div");    
+    var item = document.createElement('div');    
     item.innerHTML = `  
     <div class="nuevaLista"><img src="${dataJson.img}" alt="" width="150">
         <div id="nuevoItem">
             <p>${dataJson.producto} </p>
             <p>$${new Intl.NumberFormat('es-ar').format(dataJson.valor)} </p>
             <p>Cantidad: ${dataJson.cantidad}  </p>
-            <p>Subtotal: $<span name="subtotalProducto">${new Intl.NumberFormat('es-ar').format(subtotal)}</span></p>
+            <p>Subtotal: $<span name="subtotalProducto">${Number(subtotal)}</span></p>
         </div> <hr>
         </div>`;
         listaJson.appendChild(item);
         })
+    calcularTotal()
 }
 
 agregarElementos()
@@ -25,5 +27,17 @@ agregarElementos()
 const vaciarCarrito = document.querySelector('#botonVaciar');
 vaciarCarrito.addEventListener('click', function(){
     listaJson.innerHTML = '<h3 class="vacio">Carrito Vacío</h3><a href="index.html" class="vacioBack">Volver al shop</a>';
-    localStorage.clear()
+    localStorage.clear();
+    total.innerHTML = 'Total: $0';
 })
+
+function calcularTotal(){
+
+    let subtotales = document.getElementsByName('subtotalProducto');
+    let suma = 0;
+
+    subtotales.forEach((subtotal) => {
+        suma = suma + Number(subtotal.innerText);
+        total.innerHTML = `Total: $${new Intl.NumberFormat('es-ar').format(Number(suma))}`;
+    });
+}
